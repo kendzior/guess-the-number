983bac0fceda023134665eb41c55171bc6c016be
Michał <34143067+kendzior@users.noreply.github.com>
2018-01-14 11:57:43 +0100

    Add files via upload
    First version that gets into infinite loop and I can't find out why. A little help would be appreciated :)

---
+++ guess_number.py
@@ -0,0 +1,25 @@
+#Zgadnij jaka to liczba
+
+
+print("\tWitaj w grze 'Jaka to liczba'!")
+print("\nMam na myśli pewną liczbę z zakresu od 1 do 100.")
+print("\nSpróbuj ją odgadnąć w dziesięciu próbach.\n")
+
+import random
+
+the_number = random.randint(1, 100)
+guess = int(input("Ta liczba to: "))
+tries = 1
+
+while guess != the_number:
+    if guess > the_number:
+        print("Za duża...")
+    else:
+        print("Za mała...")
+
+guess = int(input("Ta liczba to: "))
+tries += 1
+
+print("Odgadłeś! Ta liczba to", the_number)
+print("Do osiągnięcia sukcesu potrzebowałeś tylko", tries, "prób!\n")
+
